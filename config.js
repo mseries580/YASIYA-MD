276e176cca2e14ec4e8c5633db0f4db6e3dc7b9e
mseries580 <mseries580@gmail.com>
2025-09-09 15:32:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?Vct7FA'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~BQAHRaxJ#rcue3jVRlSRcbM-Sdxj2wykmp5WtMBOc3ycHYuv0ybs'
 };
